ce43745aa3fbce5598a3afa482a916883543b6f2
Ankush Anand <ankushanand718@gmail.com>
2023-01-29 16:45:59 +0530

    Created Header Section

--- src/sections/header/Header.jsx
+++ src/sections/header/Header.jsx
@@ -1,11 +1,36 @@
 import './header.css'
-
+import HeaderImage from '../../assets/AnkushPic.jpg'
+import data from './data'
 const Header = () => {
   return (
-    <section id="Header">
-      <h2>Header</h2>
-    </section>
-  )
+    <header id="Header">
+      <div className="container header__container">
+        <div className="header__profile">
+          <img src={HeaderImage} alt="A Photo of Ankush Anand" />
+        </div>
+        <h3>Ankush Anand</h3>
+        <p>
+          Founder of DrabDigital Services - Skilled in Developing Websites,
+          Writing Content, Digital Marketing and Love to Share my Knowledge
+          through Blogging and YouTube. Full Stack Developer and Designer
+          specializing with 6+ Years of Experience in modern design &
+          development. Experienced with all stages of the development cycle for
+          dynamic web projects. Well-versed in numerous programming languages
+          including HTML5, PHP OOP, JavaScript, CSS, MySQL. Strong background in
+          project management, Term Management and customer relations.
+        </p>
+        <div className="header__cta">
+          <a href="#contact" className='btn primary'>Let's Talk</a>
+          <a href="#portfolio" className='btn'>My Work</a>
+        </div>
+        <div className="header__socials">
+          {
+            data.map(item=> <a key={item.id} href={item.link} target="_blank" rel='noopener noreferrer'>{item.icon}</a>)
+          }
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default Header
---
+++ src/sections/header/data.js
@@ -0,0 +1,19 @@
+import {AiOutlineInstagram} from 'react-icons/ai'
+import {AiOutlineGithub} from 'react-icons/ai'
+import {AiOutlineTwitter} from 'react-icons/ai'
+import {AiOutlineFacebook} from 'react-icons/ai'
+import {AiOutlineYoutube} from 'react-icons/ai'
+import {AiOutlineLinkedin} from 'react-icons/ai'
+import {AiOutlineDribbble} from 'react-icons/ai'
+
+const data = [
+    {id: 1, link: 'https://ankus.co/linkedin', icon: <AiOutlineLinkedin/>},
+    {id: 2, link: 'https://ankus.co/github', icon: <AiOutlineGithub/>},
+    {id: 3, link: 'https://ankus.co/instagram', icon: <AiOutlineInstagram/>},
+    {id: 4, link: 'https://ankus.co/youtube', icon: <AiOutlineYoutube/>},
+    {id: 5, link: 'https://ankus.co/twitter', icon: <AiOutlineTwitter/>},
+    {id: 6, link: 'https://ankus.co/dribbble', icon: <AiOutlineDribbble/>},
+    {id: 7, link: 'https://ankus.co/facebook', icon: <AiOutlineFacebook/>}
+]
+
+export default data
