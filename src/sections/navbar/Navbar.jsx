4325351ce8b0f8866f6a55a0a62ce5bf63b6a60f
Ankush Anand <ankushanand718@gmail.com>
2023-01-29 14:39:22 +0530

    Created Navbar

--- src/sections/navbar/Navbar.jsx
+++ src/sections/navbar/Navbar.jsx
@@ -1,11 +1,23 @@
+import AnkushPic from '../../assets/AnkushPic.jpg'
 import './navbar.css'
-
+import data from './data'
+import {MdInvertColors} from 'react-icons/md'
 const Navbar = () => {
   return (
-    <section id="navbar">
-        <h2>Navbar</h2>
-    </section>
-  )
+        <nav>
+          <div className="container nav__container">
+            <a href="index.html">
+              <img src={AnkushPic} alt="Ankush Anand's Picture as Logo" className='nav__logo'/>
+            </a>
+            <ul className='nav__menu'>
+              {
+                data.map(item => <li key={item.id}><a href={item.link}>{item.title}</a></li>)
+              }
+            </ul>
+            <button id='theme__icon'><MdInvertColors/></button>
+          </div>
+        </nav>
+    )
 }
 
 export default Navbar
